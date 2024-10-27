3abf36829745a70d7f6e05b87002b02d96fc6a6a
Fernando Jojoa <luis.jojoa.q@gmail.com>
2024-10-27 17:09:31 -0500

    feat: add endpoints to test meli api directly

--- backend/src/api/routes/main.ts
+++ backend/src/api/routes/main.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import axios from 'axios';
 import { getAllItems, getItemDetails } from '@/controllers/items/main.js';
 
 
@@ -7,4 +8,34 @@ const router = express.Router();
 router.get('/items', getAllItems);
 
 router.get('/items/:id', getItemDetails);
+
+router.get('/categories/:id', async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const itemResponse = await axios.get(`https://api.mercadolibre.com/categories/${id}`);
+
+    res.json({
+      category: itemResponse.data
+    });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+router.get('/items-meli/:id', async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const itemResponse = await axios.get(`https://api.mercadolibre.com/items/${id}`);
+
+    const itemData = itemResponse.data;
+
+    res.json({
+      item: itemData
+    });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
 export default router;
