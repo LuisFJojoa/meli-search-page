475d731ac7f846c728aef529c6ec0f03666c3600
Fernando Jojoa <luis.jojoa.q@gmail.com>
2024-10-28 18:51:59 -0500

    add frontend folder and add initial setup to create react app

--- backend/src/core/env/variables.ts
+++ backend/src/core/env/variables.ts
@@ -7,4 +7,4 @@ export const NODE_PORT: number = envToNumber(process.env.PORT) || 3000;
 
 export const NODE_ENV: string = getEnv(process.env.NODE_ENV) || '';
 
-export const BASE_URL: string = getEnv(process.env.BASE_URL) || '';
+export const BASE_URL: string = process.env.BASE_URL || '';
