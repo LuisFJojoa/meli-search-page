17d35ff0fddc7c9b3385d5cfd1e1392d833ff70b
Fernando Jojoa <luis.jojoa.q@gmail.com>
2024-10-28 22:01:22 -0500

    add endpoint to get details from item based on query param

--- frontend/src/modules/itemDetail/page/index.tsx
+++ frontend/src/modules/itemDetail/page/index.tsx
@@ -1,7 +1,45 @@
+import ItemsRepository from '@/services'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 export const ItemDetail = () => {
 	const { id } = useParams()
 
-	return <p>Detalle del Producto con ID: {id}</p>
+	const [item, setItems] = useState({})
+	const [loading, setLoading] = useState(true)
+	const [error, setError] = useState(null)
+
+	const itemsRepository = new ItemsRepository()
+
+	useEffect(() => {
+		const fetchItemDetails = async () => {
+			try {
+				setLoading(true)
+				const result = await itemsRepository.getItemDetails(id)
+
+				setItems(result.data.item)
+			} catch (err) {
+				setError(err.message)
+			} finally {
+				setLoading(false)
+			}
+		}
+
+		if (id) {
+			fetchItemDetails()
+		}
+	}, [id])
+
+	if (loading) return <div>Cargando...</div>
+	if (error) return <div>Error: {error}</div>
+
+	return (
+		<div>
+			<h2>Resultados para item con id: {id}</h2>
+			<p>{item.id}</p>
+			<p>{item.title}</p>
+			<img src={item.picture} alt='' srcset='' />
+			<p>{item.description}</p>
+		</div>
+	)
 }
