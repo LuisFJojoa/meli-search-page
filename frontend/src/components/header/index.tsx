a3e7be3ad2bcc7f96b7ff0eee71d302d140abefc
Fernando Jojoa <luis.jojoa.q@gmail.com>
2024-11-01 07:08:04 -0500

    feat: add pre processor compiler to avoid warnings and modify sass file name to render styles correctly

--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -1,25 +1,25 @@
-import React, { useState } from 'react';
-import './header.module.scss';
+import React, { useState } from 'react'
+import './header.scss'
 
 export const Header = () => {
-  const [searchTerm, setSearchTerm] = useState('');
+	const [searchTerm, setSearchTerm] = useState('')
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(event.target.value);
-  };
+	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setSearchTerm(event.target.value)
+	}
 
-  return (
-    <header className="header">
-      <div className="header-content">
-        <h1>Mi Aplicación</h1>
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={handleInputChange}
-          placeholder="Buscar..."
-          className="search-input"
-        />
-      </div>
-    </header>
-  );
-};
+	return (
+		<header className='header'>
+			<div className='header__content'>
+				<h1 className='header__content__title'>Mi Aplicación</h1>
+				<input
+					type='text'
+					value={searchTerm}
+					onChange={handleInputChange}
+					placeholder='Buscar...'
+					className='header__content__search-input'
+				/>
+			</div>
+		</header>
+	)
+}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,5 +10,13 @@ export default defineConfig({
     alias: {
       '@': '/src'
     }
+  },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler'
+      }
+    }
   }
 })
