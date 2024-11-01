8cb19caf32fa520a8534af176b6f6876476d5704
Fernando Jojoa <luis.jojoa.q@gmail.com>
2024-11-01 09:48:54 -0500

    feat: modify styles to change background color and add logo

--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import searchIcon from '@/assets/icons/ic_Search.png'
+import logoIcon from '@/assets/icons/Logo_ML.png'
 import './header.scss'
 
 export const Header = () => {
@@ -12,8 +13,14 @@ export const Header = () => {
 	return (
 		<header className='header'>
 			<section className='header__content'>
-				<h1 className='header__content__title'>Mi Aplicación</h1>
-				<div className='header__content__search-container'>
+				<section className='header__content__logo-container'>
+					<img
+						src={logoIcon}
+						alt='logo-icon'
+						className='header__content__logo-container__logo'
+					/>
+				</section>
+				<section className='header__content__search-container'>
 					<input
 						type='text'
 						value={searchTerm}
@@ -21,14 +28,14 @@ export const Header = () => {
 						placeholder='Nunca dejes de buscar...'
 						className='header__content__search-container__input'
 					/>
-					<div className='header__content__search-container__icon-container'>
+					<section className='header__content__search-container__icon-container'>
 						<img
 							src={searchIcon}
 							alt='searh-icon'
 							className='header__content__search-container__icon-container__icon'
 						/>
-					</div>
-				</div>
+					</section>
+				</section>
 			</section>
 		</header>
 	)
