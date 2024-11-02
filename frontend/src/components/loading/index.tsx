2c4d54d0f6779b5ad35ec5d227fbec8a6722b06f
Fernando Jojoa <luis.jojoa.q@gmail.com>
2024-11-01 21:17:14 -0500

    feat: add lazy loading and component to render while static content is downloading

--- frontend/src/components/loading/index.tsx
+++ frontend/src/components/loading/index.tsx
@@ -1,14 +1,15 @@
 import { RotatingLines } from 'react-loader-spinner'
-
+import './loading.scss'
 export const Loading = () => {
 	return (
-		<RotatingLines
-      strokeColor="#666666"
-      strokeWidth="5"
-      animationDuration="0.75"
-      width="96"
-      visible={true}
-    />
+		<section className='loading-container'>
+			<RotatingLines
+				strokeColor='#666666'
+				strokeWidth='5'
+				animationDuration='0.75'
+				width='96'
+				visible={true}
+			/>
+		</section>
 	)
 }
-
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,23 +1,43 @@
-import { ItemLayout } from '@/layouts/item/itemLayout'
-import { MainLayout } from '@/layouts/main/mainLayout'
-import { ItemDetail } from '@/modules/itemDetail/page'
-import { SearchResults } from '@/modules/searchResults/page'
-import { ReactElement } from 'react'
+import { Loading } from '@/components/loading'
+import { ReactElement, Suspense, lazy } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+const ItemLayout = lazy(() =>
+	import('@/layouts/item/itemLayout').then((module) => ({
+		default: module.ItemLayout
+	}))
+)
+const MainLayout = lazy(() =>
+	import('@/layouts/main/mainLayout').then((module) => ({
+		default: module.MainLayout
+	}))
+)
+const ItemDetail = lazy(() =>
+	import('@/modules/itemDetail/page').then((module) => ({
+		default: module.ItemDetail
+	}))
+)
+const SearchResults = lazy(() =>
+	import('@/modules/searchResults/page').then((module) => ({
+		default: module.SearchResults
+	}))
+)
 
 export default function AppRoutes(): ReactElement {
 	return (
 		<Router>
-			<Routes>
-				<Route path='*' element={<>Page not found</>} />
-				<Route path='/' element={<MainLayout />}>
-					<Route path='' element={<>Home</>} index />
-					<Route path='items' element={<ItemLayout/>}>
-						<Route element={<SearchResults />} index />
-						<Route element={<ItemDetail />} path=':id' />
+			<Suspense fallback={<Loading />}>
+				<Routes>
+					<Route path='*' element={<>Page not found</>} />
+					<Route path='/' element={<MainLayout />}>
+						<Route path='' element={<>Home</>} index />
+						<Route path='items' element={<ItemLayout />}>
+							<Route element={<SearchResults />} index />
+							<Route element={<ItemDetail />} path=':id' />
+						</Route>
 					</Route>
-				</Route>
-			</Routes>
+				</Routes>
+			</Suspense>
 		</Router>
 	)
 }
